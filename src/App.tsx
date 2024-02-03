aa656654ad38282b137f0403b9391fbfa265c5f9
manuelmartin-developer <mmartindj@gmail.com>
2024-02-03 12:36:05 +0100

    fix: Update App component, package dependencies and format script

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,95 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Layout from "./components/layout/Layout";
+import Stepper from "./components/stepper/Stepper";
+import Camera, { DocumentSide } from "./components/media/Camera";
+import Button from "./components/button/Button";
+import { useDetectedFilesStore } from "./store/detectedFiles";
+
+interface StepsProps {
+  step: number;
+  title: string;
+  description: string | React.ReactNode;
+}
 
 const App = () => {
-  /*
-   * Check if the user prefers dark mode and set the theme accordingly
-   */
+  // Store states
+  const { frontDocument, backDocument } = useDetectedFilesStore();
+  // Constants
+  const steps: StepsProps[] = [
+    {
+      step: 0,
+      title: "Welcome​",
+      description: (
+        <div>
+          <p>Welcome to the document scanner app</p>
+          <p>In this app you will be able to scan your documents</p>
+          <p>Click next to start the process</p>
+        </div>
+      ),
+    },
+    {
+      step: 2,
+      title: "Front document",
+      description: "Please take a picture of the front of your document",
+    },
+    {
+      step: 3,
+      title: "Back document",
+      description: "Please take a picture of the back of your document",
+    },
+    {
+      step: 4,
+      title: "Finish",
+      description: "You have completed the process",
+    },
+  ];
+
+  //   Component states
+  const [activeStep, setActiveStep] = useState<number>(0);
+
+  // Component Lifecycle
   useEffect(() => {
     const prefersDarkMode = window.matchMedia(
-      "(prefers-color-scheme: dark)"
+      "(prefers-color-scheme: dark)",
     ).matches;
     if (prefersDarkMode) {
       document.body.setAttribute("data-theme", "dark");
     }
   }, []);
 
+  console.log(activeStep, frontDocument, backDocument);
   return (
-    <>
-      <h1>Test</h1>
-    </>
+    <Layout>
+      <Stepper activeStep={activeStep} setActiveStep={setActiveStep}>
+        {steps.map((step) => (
+          <div key={step.step}>
+            <span>{step.title}</span>
+          </div>
+        ))}
+      </Stepper>
+      <Button
+        text="Next"
+        onClick={() => setActiveStep(activeStep + 1)}
+        disabled={activeStep === steps.length - 1}
+      />
+      <div>
+        {steps[activeStep].description}
+        {activeStep === 1 && <Camera side={DocumentSide.FRONT} />}
+        {activeStep === 2 && <Camera side={DocumentSide.BACK} />}
+        {activeStep === 3 && frontDocument && backDocument && (
+          <>
+            <img
+              src={`data:image/png;base64,${frontDocument}`}
+              alt="Front document"
+            />
+            <img
+              src={`data:image/png;base64,${backDocument}`}
+              alt="Back document"
+            />
+          </>
+        )}
+      </div>
+    </Layout>
   );
 };
 
